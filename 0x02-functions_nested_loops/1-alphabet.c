2441880f2830df0a66793abae3ac6cb9308045d8
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-14 04:49:43 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,9 @@
-#include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0(Success)
+#include <"main.h"
+/**i
+ * print alphabet in lowercase
  */
-void print_alphabet(void);
-
-int  main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
-	/**
-	  * print_alphabet - print alphabets in lowercase
-	  *
-	  * Return: no reurn value
-	  */
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
