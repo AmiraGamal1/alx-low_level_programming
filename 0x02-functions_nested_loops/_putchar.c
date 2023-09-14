9fb59200d6cfa7997ff39f9c3e43dbd2ac4efd6f
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-14 04:22:00 +0300

    putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+  *_putchar - write character c to stdout
+  *
+  * Return: On success 1.
+  */#include "main.h"
+#include <unistd.h>
+/**
+  _putchar - write character c to stdout
+  *
+  * Return: On success 1.
+  */
