2f1b876404d744b3da2a01ce74c5fb0913201bcc
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-14 03:18:25 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
-/** 
- * min - Entry point
+/**
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
+	printf("_putchar\n");
+	return (0);
 }
-
