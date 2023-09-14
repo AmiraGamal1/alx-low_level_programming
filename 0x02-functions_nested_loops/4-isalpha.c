4c0520607211e290bb28994ea69ae42e89c5c5dc
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-14 05:39:39 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,28 +1,10 @@
-#include <stdio.h>
-
+#include"main.h"
 /**
-  * main - Entry point
-  *
-  * Return: Always 0.
-  */
-int _isalpha(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	putchar(r + '0');
-	r = _isalpha('o');
-	putchar(r + '0');
-	r = _isalpha(108);
-	putchar(r + '0');
-	r = _isalpha(';');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
-
+ * _islower - tests whether a character is a
+ * lowercase letter from the English alphabet.
+ * @c: character to test.
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ */
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
