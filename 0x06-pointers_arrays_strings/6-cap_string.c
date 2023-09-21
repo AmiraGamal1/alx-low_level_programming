ed5917ab602a19907e0fd74615f01099575de7b3
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-21 15:26:31 +0300

    6. Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * cap_string - capitalize the words
+  * @a: char pointer
+  * Return: char pointer
+  */
+char *cap_string(char *a)
+{
+	int i, j;
+	char sep[] = {' ', '\n', '\t', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}'};
+	
+	i = 0;
+	while (a[i] != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (a[i] == sep[j])
+			{
+				if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+				{
+					a[i + 1] = a[i + 1] - 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (a);
+}
