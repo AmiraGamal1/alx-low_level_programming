6dfa9cf43ff7473664a2cf9ac01709bda5d18b96
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-28 11:38:49 +0300

    print string in reverse order

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - print string in reverse order
+ * @s: char to be print
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+		_print_rev_recursion(s + 1);
+	if (*s == '\0')
+		_putchar('\n');
+	else
+		_putchar(*s);
+}
