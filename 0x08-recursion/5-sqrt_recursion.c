08bc558726feaba9d96e72d135988c23491bce8a
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-09-28 19:58:00 +0300

    solve long int multiplication

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,21 +8,25 @@
   * @end: end point
   * Return: square root
   */
-int _recursion(int n, int start, int end)
+int _recursion(int n, long int start, long int end)
 {
-	int mid;
+	long int mid;
+	long int mul;
 
-	if (start <= end)
+	if (start > end)
+		return ((int) end);
+	mid = (start + end) / 2;
+	mul = mid * mid;
+	if (mul == n)
+		return ((int) mid);
+	else if (mul < n)
 	{
-		mid = (start + end) / 2;
-		if (mid * mid == n)
-			return (mid);
-		if (mid * mid < n)
-			return _recursion(n, mid + 1, end);
-		else
-			return _recursion(n, start, mid - 1);
+		return (_recursion(n, mid + 1, end));
 	}
-	return (-1);
+	else
+	{
+		return (_recursion(n, start, mid - 1));
+	}
 }
 /**
   * _sqrt_recursion - count the natural spure root
@@ -31,10 +35,10 @@ int _recursion(int n, int start, int end)
   */
 int _sqrt_recursion(int n)
 {
-	if (n == 1 || n == 0)
-		return (n);
 	if (n < 0)
 		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
 	else
-		return (_recursion(n, 1, n));
+		return (_recursion(n, 1, n / 2));
 }
