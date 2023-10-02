f9de19d2b5a52da22f6ee1fe3ec24f66727143e0
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-10-03 01:35:44 +0300

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,38 +1,5 @@
 #include <stdio.h>
-
-/**
-  * _atoi - convert str to int
-  * @s: string
-  * Return: int
-  */
-int _atoi(char *s)
-{
-	int result = 0;
-	int sign = 1;
-	int i = 0;
-
-	/*Skip leadind space and alphabets*/
-	while (!(s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
-	{
-		i++;
-	}
-	if (s[i] == '\0')
-	{
-		return (0);
-	}
-	/*check rhe char before digirs if it -/+*/
-	if (s[i - 1] == '-' || s[i - 1] == '+')
-	{
-		sign = (s[i - 1] == '-') ? -1 : 1;
-	}
-	/*convert digit to intger*/
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i++] - '0');
-	}
-	return (sign * result);
-}
-
+#include <stdlib.h>
 /**
   * main - Entry point
   * @argc: num of arg
@@ -50,8 +17,8 @@ int main(int argc, char *argv[])
 	{
 		int a, b;
 
-		a = _atoi(argv[1]);
-		b = _atoi(argv[2]);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
 		printf("%d\n", a * b);
 	}
 	return (0);
