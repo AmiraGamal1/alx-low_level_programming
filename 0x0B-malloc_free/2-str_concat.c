1b028e8597885d7ed3dad72ae9c55dcc835e9b94
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-10-03 17:54:37 +0300

    handdle null string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,16 +11,10 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int i = 0, j = 0, size1 = 0, size2 = 0;
-	char *str, *s;
-
-	*s = '\0';
+	char *str;
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		s1 = s;
-	if (s2 == NULL)
-		s2 = s;
 
 	while (s1[size1] != '\0')
 		size1++;
@@ -30,16 +24,21 @@ char *str_concat(char *s1, char *s2)
 	str = malloc(size1 + size2 + 1);
 	if (str == NULL)
 		return (0);
-
-	while (i != size1)
+	if (s1)
 	{
-		str[i] = s1[i];
-		i++;
+		while (i != size1)
+		{
+			str[i] = s1[i];
+			i++;
+		}
 	}
-	while (j != size2)
+	if (s2)
 	{
-		str[i + j] = s2[j];
-		j++;
+		while (j != size2)
+		{
+			str[i + j] = s2[j];
+			j++;
+		}
 	}
 	str[i + j] = '\0';
 
