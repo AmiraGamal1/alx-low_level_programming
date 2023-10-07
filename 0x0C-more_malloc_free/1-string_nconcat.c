7ad888fdcf5de6c0f52c57e9a2a5f9a3650f60e2
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-10-07 16:43:43 +0300

    strlen

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ unsigned int _strlen(char *str)
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2, i, j, size;
+	unsigned int l1, l2, i = 0, j = 0, size = 0;
 	char *str;
 
 	l1 = _strlen(s1);
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		size = l1 + l2 + 1;
 	else
 		size = l1 + n + 1;
-	str = malloc(sizeof(size) * size);
+	str = malloc(sizeof(char) * size);
 	if (str == NULL)
 		return (NULL);
 	if (s1)
