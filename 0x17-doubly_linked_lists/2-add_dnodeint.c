80c95ca23a8922e0324073a77eff803c32781480
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-09 01:21:09 +0200

    2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * add_dnodeint - add new node
+  * @head: head
+  * @n: int
+  * Return: the address of the new node
+  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	h = *head;
+	new->n = n;
+	new->next = h;
+	new->prev = NULL;
+	if (h != NULL)
+		h->prev = new;
+	h = new;
+
+	return (h);
+}
