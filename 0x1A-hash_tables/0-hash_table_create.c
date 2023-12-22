2e032b3d0dafeeaeab0f8603d7d7fdb1baa6dd90
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-22 10:52:54 +0200

    1-djb2.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
@@ -19,6 +20,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
