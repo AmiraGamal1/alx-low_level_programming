ea610ca8e1a57a7cd486603525eeb11b04d5f910
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-22 10:39:51 +0200

    fix error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
@@ -20,8 +19,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
 
 	return (table);
 }
