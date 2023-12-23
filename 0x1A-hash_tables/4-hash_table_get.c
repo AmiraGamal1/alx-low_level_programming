585aa1e94df57fcdb0fff2316333e2f322e5728e
Amira Nasr Aldein <amiragamal098@gmail.com>
2023-12-23 08:24:41 +0200

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,4 +1,23 @@
 #include "hash_tables.h"
 
 /**
-  * 
+  * hash_table_get - retrieves a value associated with a key
+  * @ht: hash table
+  * @key: the key
+  * Return: value or NULL
+  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
